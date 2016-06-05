a6dcbc77d85c36675551a47c8e6ed03bbd2a338e
Jae <jaecarbary@gmail.com>
2016-06-05 23:39:17 +0000

    Added links and edit, update, show, and index pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,41 @@
 class ArticlesController < ApplicationController
   
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice]= "The article was updated successfully."
+      redirect_to article_path(@article)
+    else
+      #flash[:notice]= "there was a problem."
+      render 'edit'
+    end
+  end
+  
   def create
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice]= "The article was saved successfully."
+      redirect_to article_path(@article)
+    else
+      #flash[:notice]= "there was a problem."
+      render 'new'
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
