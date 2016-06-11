f7fb4b467f88f2b4d8b2033a026a983c48f6d249
Jae <jaecarbary@gmail.com>
2016-06-11 15:04:25 +0000

    associate users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,6 +26,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:success]= "The article was saved successfully."
       redirect_to article_path(@article)
