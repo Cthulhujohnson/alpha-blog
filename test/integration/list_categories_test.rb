f70afc173ab0876a8181071b29e4594ecb4e5900
Jae <jaecarbary@gmail.com>
2016-06-18 21:14:40 +0000

    expanded admin functionality and navigation

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 class ListCategoriesTest < ActionDispatch::IntegrationTest
   
   def setup
-    @category = Category.new(name: "bloops")
+    @category = Category.new(name: "foods")
     @category2 = Category.new(name: "programming")
   end
   
-  test "test should show categories listing" do
+  test "should show categories listing" do
     get categories_path
     assert_template 'categories/index'
     assert_select "a[href=?]", categories_path(@category), text: @category.name
