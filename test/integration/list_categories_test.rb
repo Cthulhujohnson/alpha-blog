331e751f8a407dd057415f4a90adcedcc1ab1ba3
Jae <jaecarbary@gmail.com>
2016-06-18 20:39:00 +0000

    added admin functionallity to categories

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -3,15 +3,15 @@ require 'test_helper'
 class ListCategoriesTest < ActionDispatch::IntegrationTest
   
   def setup
-    @category = Category.new("cds")
-    @category2 = Category.new("programming")
+    @category = Category.new(name: "bloops")
+    @category2 = Category.new(name: "programming")
   end
   
   test "test should show categories listing" do
     get categories_path
     assert_template 'categories/index'
-    assert_select "a[href=?]", category_path(@category), text: @category.name
-    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+    assert_select "a[href=?]", categories_path(@category), text: @category.name
+    assert_select "a[href=?]", categories_path(@category2), text: @category2.name
   end
   
 end
